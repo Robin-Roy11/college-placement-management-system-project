1930c2ed0ebc2cbaa84c288bdca74a78024aa3f2
Robin-Roy11 <robinroy.ax@gmail.com>
2025-06-07 16:34:30 +0530

    final port changed

--- frontend/src/config/backend_url.jsx
+++ frontend/src/config/backend_url.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://college-placement-management-system-1wwe.onrender.com/';
-//export const BASE_URL = 'https://cpms-api.vercel.app';
+//export const BASE_URL = 'https://college-placement-management-system-1wwe.onrender.com/';
+export const BASE_URL = 'http://localhost:3001'; // Uncomment this line for local development
